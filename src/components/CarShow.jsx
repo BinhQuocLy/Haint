fb024d8c9903a3555e59dc2425271103465f151e
Amber <abc@def.com>
2023-10-03 15:27:43 +0700

    Update first car

--- src/components/CarShow.jsx
+++ src/components/CarShow.jsx
@@ -4,16 +4,37 @@ import React from 'react'
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 
 import "@/styles/global.css"
+import Ground from './Ground'
+import Car from './Car'
 
 const CarShow = () => {
   return (
     <>
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color={"red"} />
-      </mesh>
+
+      <color args={[0, 0, 0]} attach="background" />
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={100.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={200}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Ground />
+      <Car />
     </>
   )
 }
